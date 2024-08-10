beacf7e24c0bf8ec1eb95fa109997eedbb75f4e1
Fallougin <artursuvor@mail.ru>
2024-08-10 18:48:49 +0200

    cart done (redux)

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,13 @@
+// src/app/cart/page.tsx
+import React from 'react';
+import CartClient from '@/components/CartClient';
+
+const Cart = () => {
+  return (
+    <div>
+      <CartClient />
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useState, FormEvent } from 'react';
+import Link from 'next/link';
+
+const Login = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = (e: FormEvent) => {
+    e.preventDefault();
+    // Handle login logic here
+    console.log('Login details:', { email, password });
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100 dark:bg-gray-800">
+      <form onSubmit={handleLogin} className="bg-white dark:bg-gray-700 p-8 rounded shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-bold mb-6 text-gray-800 dark:text-gray-200">Login</h2>
+        <div className="mb-4">
+          <label className="block text-gray-700 dark:text-gray-300 mb-2" htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full p-2 border border-gray-300 dark:border-gray-600 rounded bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100"
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 dark:text-gray-300 mb-2" htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-2 border border-gray-300 dark:border-gray-600 rounded bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded"
+        >
+          Login
+        </button>
+        <Link href="/register" className="text-blue-500 hover:underline">Register</Link>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
